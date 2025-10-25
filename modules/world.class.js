82a0b1823e8351a05d162c5cd605b1bff3be3579
Lee-Roy Romann <104465642+lee-royromann@users.noreply.github.com>
2025-10-25 22:16:28 +0200

    feat: add sky layer to background
    - Add air.png as background sky layer

--- modules/world.class.js
+++ modules/world.class.js
@@ -11,6 +11,7 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject("/img/5_background/layers/air.png"),
         new BackgroundObject("/img/5_background/layers/3_third_layer/1.png"),
         new BackgroundObject("/img/5_background/layers/2_second_layer/1.png"),
         new BackgroundObject("/img/5_background/layers/1_first_layer/1.png")
