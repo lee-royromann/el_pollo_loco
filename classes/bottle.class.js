19e9435ef0d992f81b5de084084b9a2d8b1c22d4
Lee-Roy Romann <104465642+lee-royromann@users.noreply.github.com>
2025-12-02 21:30:30 +0100

    docs: add JSDoc to Bottle class
    Document class, constructor and method descriptions.

--- classes/bottle.class.js
+++ classes/bottle.class.js
@@ -1,9 +1,18 @@
+/**
+ * A collectible bottle on the ground.
+ * @extends MovableObject
+ */
 class Bottle extends MovableObject {
     IMAGES_BOTTLE = [
         "./img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
         "./img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
     ];
 
+    /**
+     * Creates a bottle at the given position.
+     * @param {number} x - X position of the bottle.
+     * @param {number} y - Y position of the bottle.
+     */
     constructor(x, y) {
         super().loadImage("./img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
         this.loadImages(this.IMAGES_BOTTLE);
@@ -20,6 +29,9 @@ class Bottle extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Starts the bottle animation.
+     */
     animate() {
         setInterval(() => {
             this.playAnimation(this.IMAGES_BOTTLE);
