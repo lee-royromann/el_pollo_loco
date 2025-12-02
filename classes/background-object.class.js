11063dac427f79f0f1c5181839e0140218701337
Lee-Roy Romann <104465642+lee-royromann@users.noreply.github.com>
2025-12-02 21:28:21 +0100

    docs: add JSDoc to BackgroundObject class
    Document class description and constructor parameters.

--- classes/background-object.class.js
+++ classes/background-object.class.js
@@ -1,5 +1,14 @@
+/**
+ * A background layer image.
+ * @extends MovableObject
+ */
 class BackgroundObject extends MovableObject {
 
+    /**
+     * Creates a background object.
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - X position of the background.
+     */
     constructor(imagePath, x) {
         super();
         this.loadImage(imagePath);
