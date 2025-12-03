236e5d881d13ef328d101b97cda726f66cb07576
Lee-Roy Romann <104465642+lee-royromann@users.noreply.github.com>
2025-12-03 08:57:40 +0100

    docs: add JSDoc to Level class
    Document class and constructor descriptions.

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,3 +1,6 @@
+/**
+ * A game level with enemies, clouds, coins and bottles.
+ */
 class Level {
     enemies;
     clouds;
@@ -5,6 +8,13 @@ class Level {
     bottles;
     level_end_x = 5000;
 
+    /**
+     * Creates a new level.
+     * @param {MovableObject[]} enemies - Array of enemy objects.
+     * @param {Cloud[]} clouds - Array of cloud objects.
+     * @param {Coin[]} coins - Array of coin objects.
+     * @param {Bottle[]} bottles - Array of bottle objects.
+     */
     constructor(enemies, clouds, coins, bottles) {
         this.enemies = enemies;
         this.clouds = clouds;
